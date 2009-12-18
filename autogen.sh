ac54908d7e623dcdfdd7e1260bd9e7796f4a03f7
Mirek Kratochvil <exa.exa@gmail.com>
2009-12-18 11:37:52 +0100

    better autogen

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+COMMON_CPPFLAGS="-Iinclude/ -I/usr/local/include"
+COMMON_LDFLAGS="-L/usr/local/libs"
+
 OUT=Makefile.am
 touch NEWS AUTHORS ChangeLog
 echo > $OUT
@@ -9,14 +13,14 @@ COMMON=`echo common/*.cpp`
 echo "bin_PROGRAMS = ${PROGS}" >>$OUT
 echo "noinst_LIBRARIES = libcommon.a" >>$OUT
 echo "libcommon_a_SOURCES = $COMMON" >>$OUT
-echo "libcommon_a_CPPFLAGS = -Iinclude/" >>$OUT
+echo "libcommon_a_CPPFLAGS = ${COMMON_CPPFLAGS}" >>$OUT
 
 for i in $PROGS ; do
 	SOURCES=`echo src/$i/*.cpp`
 	echo "${i}_SOURCES = $SOURCES" >>$OUT
-	echo "${i}_CPPFLAGS = -Isrc/$i/ -Iinclude/" >>$OUT
+	echo "${i}_CPPFLAGS = -Isrc/$i/ ${COMMON_CPPFLAGS}" >>$OUT
 	echo "${i}_LDADD = libcommon.a" >>$OUT
-	echo "${i}_LDFLAGS = " >>$OUT #empty for future use.
+	echo "${i}_LDFLAGS = ${COMMON_LDFLAGS}" >>$OUT #empty for future use.
 	[ -f src/$i/Makefile.am.extra ] &&
 		while read l ; do
 			[ "$l" ] && echo "${i}_${l}" >>$OUT
