9545a2b9b0867cf181031c597d484bf755aa5d12
Mirek Kratochvil <exa.exa@gmail.com>
2009-12-19 12:57:34 +0100

    more connectors

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-COMMON_CPPFLAGS="-Iinclude/ -I/usr/local/include"
+COMMON_CPPFLAGS="-Icommon/ -I/usr/local/include"
 COMMON_LDFLAGS="-L/usr/local/libs"
 
 OUT=Makefile.am
@@ -9,15 +9,18 @@ echo > $OUT
 cd src
 PROGS=`echo *`
 cd ..
-COMMON=`echo common/*.cpp`
+
 echo "bin_PROGRAMS = ${PROGS}" >>$OUT
 echo "noinst_LIBRARIES = libcommon.a" >>$OUT
-echo "libcommon_a_SOURCES = $COMMON" >>$OUT
+echo "libcommon_a_SOURCES = `echo common/*.cpp`" >>$OUT
+echo "noinst_HEADERS = `echo common/*.h`" >>$OUT
+echo "libcommon_adir = common/" >>$OUT
 echo "libcommon_a_CPPFLAGS = ${COMMON_CPPFLAGS}" >>$OUT
 
 for i in $PROGS ; do
-	SOURCES=`echo src/$i/*.cpp`
-	echo "${i}_SOURCES = $SOURCES" >>$OUT
+	echo "${i}dir = src/$i/" >>$OUT
+	echo "${i}_SOURCES = `echo src/$i/*.cpp`" >>$OUT
+	echo "noinst_HEADERS += `echo src/$i/*.h |grep -v '*'`" >>$OUT
 	echo "${i}_CPPFLAGS = -Isrc/$i/ ${COMMON_CPPFLAGS}" >>$OUT
 	echo "${i}_LDADD = libcommon.a" >>$OUT
 	echo "${i}_LDFLAGS = ${COMMON_LDFLAGS}" >>$OUT #empty for future use.
