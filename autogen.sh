791446880786a16e5b39071d6ad2a29fc09246d8
Mirek Kratochvil <exa.exa@gmail.com>
2009-12-19 13:12:07 +0100

    autogen + autoconf match new layout

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# simple autogen script that generates basic layout for autotools.
+# not meant to be included in distribution.
+
 COMMON_CPPFLAGS="-Icommon/ -I/usr/local/include"
 COMMON_LDFLAGS="-L/usr/local/libs"
 
@@ -8,9 +11,12 @@ touch NEWS AUTHORS ChangeLog
 echo > $OUT
 cd src
 PROGS=`echo *`
+cd ../connectors
+CONNECTORS=`echo *`
 cd ..
 
 echo "bin_PROGRAMS = ${PROGS}" >>$OUT
+echo "lib_LTLIBRARIES = `for i in ${CONNECTORS}; do echo -n \"lib$i.la \" ; done`" >>$OUT
 echo "noinst_LIBRARIES = libcommon.a" >>$OUT
 echo "libcommon_a_SOURCES = `echo common/*.cpp`" >>$OUT
 echo "noinst_HEADERS = `echo common/*.h`" >>$OUT
@@ -30,4 +36,18 @@ for i in $PROGS ; do
 		done < src/$i/Makefile.am.extra
 done
 
-aclocal && autoconf && automake --add-missing
+for i in $CONNECTORS ; do
+	echo "lib${i}_ladir = connectors/${i}" >>$OUT
+	echo "lib${i}_la_SOURCES = `echo connectors/$i/*.cpp`" >>$OUT
+	echo "noinst_HEADERS += `echo src/$i/*.h |grep -v '*'`" >>$OUT
+	echo "lib${i}_la_CPPFLAGS = -Isrc/$i/ ${COMMON_CPPFLAGS}" >>$OUT
+	echo "lib${i}_la_LIBADD = libcommon.a" >>$OUT
+	echo "lib${i}_la_LDFLAGS = ${COMMON_LDFLAGS}" >>$OUT #empty for future use.
+	[ -f src/$i/Makefile.am.extra ] &&
+		while read l ; do
+			[ "$l" ] && echo "${i}_${l}" >>$OUT
+		done < src/$i/Makefile.am.extra
+done
+
+libtoolize --force && aclocal && autoconf && automake --add-missing
+
