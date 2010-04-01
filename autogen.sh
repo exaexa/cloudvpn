5d55016c908df3874262ceda461d0062870779af
Mirek Kratochvil <exa.exa@gmail.com>
2010-04-01 23:34:54 +0200

    from the scratch again

--- autogen.sh
+++ autogen.sh
@@ -3,7 +3,7 @@
 # simple autogen script that generates basic layout for autotools.
 # not meant to be included in distribution.
 
-COMMON_CPPFLAGS="-I\$(srcdir)/common/ -I/usr/local/include"
+COMMON_CPPFLAGS="-I\$(srcdir)/include/ -I/usr/local/include"
 COMMON_LDFLAGS="-L/usr/local/libs"
 
 OUT=Makefile.am
@@ -11,34 +11,26 @@ touch NEWS AUTHORS ChangeLog
 echo > $OUT
 cd src
 PROGS=`echo *`
-cd ../connectors
-CONNECTORS=`echo *`
+cd ../plugins
+PLUGINS=`echo *`
 cd ..
 
-echo "bin_PROGRAMS = ${PROGS}" >>$OUT
-echo "pkglib_LTLIBRARIES = `for i in ${CONNECTORS}; do echo -n \"lib$i.la \" ; done`" >>$OUT
-echo "noinst_LIBRARIES = libcommon.a" >>$OUT
-echo "libcommon_a_SOURCES = `echo common/*.cpp`" >>$OUT
-echo "noinst_HEADERS = `echo common/*.h`" >>$OUT
-echo "libcommon_adir = common/" >>$OUT
-echo "libcommon_a_CPPFLAGS = ${COMMON_CPPFLAGS}" >>$OUT
+echo "bin_PROGRAMS = cloudvpn" >>$OUT
+echo "pkglib_LTLIBRARIES = `for i in ${PLUGINS}; do echo -n \"lib$i.la \" ; done`" >>$OUT
+echo "noinst_HEADERS = `echo include/*.h`" >>$OUT
 
-for i in $PROGS ; do
-	echo "${i}dir = src/$i/" >>$OUT
-	echo "${i}_SOURCES = `echo src/$i/*.cpp`" >>$OUT
-	echo "noinst_HEADERS += `echo src/$i/*.h |grep -v '*'`" >>$OUT
-	echo "${i}_CPPFLAGS = -I\$(srcdir)/src/$i/ ${COMMON_CPPFLAGS}" >>$OUT
-	echo "${i}_LDADD = libcommon.a" >>$OUT
-	echo "${i}_LDFLAGS = ${COMMON_LDFLAGS}" >>$OUT #empty for future use.
-	[ -f src/$i/Makefile.am.extra ] &&
-		while read l ; do
-			[ "$l" ] && echo "${i}_${l}" >>$OUT
-		done < src/$i/Makefile.am.extra
-done
+echo "cloudvpndir = src/" >>$OUT
+echo "cloudvpn_SOURCES = `echo src/*.c`" >>$OUT
+echo "cloudvpn_CPPFLAGS = ${COMMON_CPPFLAGS}" >>$OUT
+echo "cloudvpn_LDFLAGS = ${COMMON_LDFLAGS}" >>$OUT #empty for future use.
+[ -f src//Makefile.am.extra ] &&
+	while read l ; do
+		[ "$l" ] && echo "cloudvpn_${l}" >>$OUT
+	done < src/Makefile.am.extra
 
-for i in $CONNECTORS ; do
-	echo "lib${i}_ladir = connectors/${i}" >>$OUT
-	echo "lib${i}_la_SOURCES = `echo connectors/$i/*.cpp`" >>$OUT
+for i in $PLUGINS ; do
+	echo "lib${i}_ladir = plugins/${i}" >>$OUT
+	echo "lib${i}_la_SOURCES = `echo plugins/$i/*.c`" >>$OUT
 	echo "noinst_HEADERS += `echo src/$i/*.h |grep -v '*'`" >>$OUT
 	echo "lib${i}_la_CPPFLAGS = -I\$(SRCDIR)/src/$i/ ${COMMON_CPPFLAGS}" >>$OUT
 	echo "lib${i}_la_LDFLAGS = ${COMMON_LDFLAGS}" >>$OUT #empty for future use.
--- src/cloud/routetree.cpp
+++
@@ -1,3 +0,0 @@
-
-#include "routetree.h"
-
