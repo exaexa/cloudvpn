fe94951412dece657f02b4ba8a4cbfbc4d7d85a1
Mirek Kratochvil <exa.exa@gmail.com>
2010-05-24 18:10:43 +0200

    more work on events
    refs #4

--- src/event.c
+++ src/event.c
@@ -17,9 +17,12 @@
 #define _XOPEN_SOURCE
 #include <ev.h>
 
-static cl_mutex event_mutex;
-static struct ev_loop* loop;
-static ev_async async;
+static void reload_event_loop();
+
+/*
+ * frontend just pushes events to the list that is processed by event core
+ * whenever it has time for it.
+ */
 
 struct event* cloudvpn_new_event() {
 	return cl_malloc (sizeof (struct event) );
@@ -30,22 +33,41 @@ void cloudvpn_delete_event (struct event*e)
 	cl_free (e);
 }
 
-static void async_callback (EV_P_ ev_async*w, int revents) {}
+typedef enum {add, remove} eventlist_op;
 
-static void notify_event_loop()
-{
-	/* asynchronously interrupt sleep so libev can update itself */
-	ev_async_send (loop, &async);
-}
+struct eventlist {
+	struct event*e;
+	struct eventlist*next;
+	eventlist_op op; /* otherwise remove */
+};
 
 void cloudvpn_register_event (struct event*e)
 {
-	notify_event_loop();
+	/*TODO*/
+	reload_event_loop();
 }
 
 void cloudvpn_unregister_event (struct event*e)
 {
-	notify_event_loop();
+	/*TODO*/
+	reload_event_loop();
+}
+
+/*
+ * event core functions
+ */
+
+
+static cl_mutex eventcore_mutex;
+static struct ev_loop* loop;
+static ev_async async;
+
+static void async_callback (EV_P_ ev_async*w, int revents) {}
+
+static void reload_event_loop()
+{
+	/* asynchronously interrupt sleep so libev can update itself */
+	ev_async_send (loop, &async);
 }
 
 int cloudvpn_event_init()
@@ -55,19 +77,25 @@ int cloudvpn_event_init()
 	ev_async_init (&async, async_callback);
 	ev_async_start (loop, &async);
 
-	return loop || cl_mutex_init (&event_mutex);
+	return loop || cl_mutex_init (&eventcore_mutex);
 }
 
 int cloudvpn_event_finish()
 {
-	return cl_mutex_destroy (event_mutex);
+	return cl_mutex_destroy (eventcore_mutex);
 }
 
 void cloudvpn_wait_for_event()
 {
 	/* don't block if there's already other thread waiting */
-	if (cl_mutex_trylock (event_mutex) ) return;
+	if (cl_mutex_trylock (eventcore_mutex) ) return;
 
-	cl_mutex_unlock (event_mutex);
+	/* load stuff from frontend (tear off and convert list to queue) */
+
+	/*TODO*/
+
+	ev_loop (loop, EVLOOP_ONESHOT);
+
+	cl_mutex_unlock (eventcore_mutex);
 }
 
